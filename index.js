92029021c5a462bc0470ba696bfdc3a2d0aa6162
gazf <gazfff@gmail.com>
2019-01-02 23:13:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const dtf = new Intl.DateTimeFormat("ja", {month: "numeric", timeZone: "asia/tokyo"});
+const dtf = new Intl.DateTimeFormat("ja-jp", {month: "numeric", timeZone: "Asia/Tokyo"});
 const fmt = dtf.format(new Date(2019, 1 - 1, 1));
 console.log(fmt);
